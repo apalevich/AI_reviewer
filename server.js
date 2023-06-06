5403a04db7593eeee2500ce635587e37d3a23896
apalevich <backstageofme@gmail.com>
2023-06-06 19:13:07 +0400

    Link form on frontend with backend

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:5173' // Allow requests from this origin
+    })
+);
 
 const PORT = 8000;
 const BASE_URL = 'https://api.github.com';
 
-app.get('/getrepo', async (req, res) => {
+app.get('/getrepo/:owner/:repo', async (req, res) => {
+    const { owner, repo } = req.params;
     try {
-        const url = new URL(req.query.url);
-        const [_, owner, repo] = url.pathname.split('/');;
-        console.log(owner, repo);
         const response = await axios.get(`${BASE_URL}/repos/${owner}/${repo}`);
         const data = response.data;
         res.json(data);
--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,38 @@
 <template>
-    <div class="w-screen h-screen bg-indigo-500 flex items-center justify-center">
-        <form action="/getrepo" method="get">
+    <div class="w-screen h-screen bg-indigo-500 flex items-center justify-center flex-col">
+        <form @submit.prevent="getRepo" class="flex flex-col">
             <label for="url">Paste link to a public GitHub repository</label>
-            <input type="url" name="url" id="url" required>
+            <input type="url" name="url" id="url" v-model="url" required placeholder="https://github.com/">
             <button type="submit">Get data</button>
         </form>
     </div>
 </template>
 <script>
+export default {
+    name: 'EntryPage',
+    data() {
+        return {
+            url: '',
+            res: {},
+        }
+    },
+    methods: {
+        async getRepo() {
+            const url = new URL(this.url);
+            const [owner, repo] = url.pathname.slice(1).split('/');
+            console.log(owner, repo);
+
+            try {
+                const response = await fetch(`http://localhost:8000/getrepo/${owner}/${repo}`);
+                const data = await response.json();
+                this.res = data;
+                console.log('data', data);
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    },
+}
 </script>
 <style scoped>
 
