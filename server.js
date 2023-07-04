df0f925ab95f849135661e69b7de78cc172b9f39
apalevich <backstageofme@gmail.com>
2023-07-04 18:46:08 +0400

    Make file content appear by click on the item in the list

--- server.js
+++ server.js
@@ -8,37 +8,58 @@ const app = express();
 app.use(express.json());
 // app.use(history());
 app.use(
-    cors({
-        origin: 'http://localhost:8080' // Allow requests from this origin
-    })
+  cors({
+    origin: 'http://localhost:8080' // Allow requests from this origin
+  })
 );
 
 const PORT = 8000;
 const BASE_URL = 'https://api.github.com';
 
 app.get('/getrepo/:owner/:repo', async (req, res) => {
-    const { owner, repo } = req.params;
-    try {
-        const response = await axios.get(`${BASE_URL}/repos/${owner}/${repo}`);
-        const data = response.data;
-        res.json(data);
-    } catch (error) {
-        console.error('error:', error);
-        res.status(500).json(error);
-    }
+  const { owner, repo } = req.params;
+  try {
+    const response = await axios.get(`${BASE_URL}/repos/${owner}/${repo}`);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('error:', error);
+    res.status(500).json(error);
+  }
 });
 
 app.get('/getcontent/:owner/:repo/', async (req, res) => {
-    const { owner, repo } = req.params;
-    try {
-        const response = await axios.get(`${BASE_URL}/repos/${owner}/${repo}/contents`);
-        const data = response.data;
-        res.json(data);
-    } catch (error) {
-        console.error('error:', error);
-        res.status(500).json(error);
-    }
+  const { owner, repo } = req.params;
+  try {
+    const response = await axios.get(`${BASE_URL}/repos/${owner}/${repo}/contents`);
+    const data = response.data;
+
+    res.json(data);
+  } catch (error) {
+    console.error('error:', error);
+
+    res.status(500).json(error);
+  }
 });
+
+app.get('/getfile/:owner/:repo/:file', async (req, res) => {
+  const { owner, repo, file } = req.params;
+  try {
+    let url = `${BASE_URL}/repos/${owner}/${repo}/contents/${file}`;
+
+    const response = await axios.get(url);
+    const data = response.data;
+
+    res.json(atob(data.content));
+  } catch (error) {
+    console.error('error:', error);
+    res.status(500).json(error);
+  }
+})
+
+async function makeRequest(url) {
+  // TODO: write abstract finction to make Axios request to the GH API
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/components/OverviewPage.vue
+++ src/components/OverviewPage.vue
@@ -4,90 +4,86 @@
     </v-app-bar>
 
     <v-navigation-drawer>
-        <div
-            v-if="repoLoading"
-            class="text-center"
-        >
-            <v-progress-circular indeterminate/>
+        <div v-if="repoLoading" class="text-center">
+            <v-progress-circular indeterminate />
         </div>
-        <v-list
-            v-else
-            v-model:opened="open"
-            open-strategy="single"
-        > 
+        <v-list v-else v-model:opened="open" open-strategy="single">
             <template v-for="item in content">
-                <v-list-group 
-                    v-if="item.type === 'dir'"
-                    :value="item.name"
-                >
+                <v-list-group v-if="item.type === 'dir'" :value="item.name">
                     <template v-slot:activator="{ props }">
-                        <v-list-item
-                            v-bind="props"
-                            :title="item.name"
-                        />
+                        <v-list-item v-bind="props" :title="item.name" />
                     </template>
                     <div class="text-center" v-if="!item.content">
                         <v-progress-circular indeterminate></v-progress-circular>
                     </div>
-                    <v-list-item
-                        v-else
-                        prepend-icon="description"
-                    />
+                    <v-list-item v-else prepend-icon="description" />
                 </v-list-group>
-                
-                <v-list-item
-                    v-if="item.type === 'file'"
-                    :key="item.sha"
-                    :value="item.name"
-                    :title="item.name"
-                    prepend-icon="description"
-                />
+
+                <v-list-item v-if="item.type === 'file'" :key="item.sha" :value="item.name" :title="item.name"
+                    prepend-icon="description" @click="() => getFileContent(item.name)" />
             </template>
         </v-list>
     </v-navigation-drawer>
 
-    <v-main >
-        <h1>{{authorName}}</h1>
-        <h2>{{repoName}}</h2>
+    <v-main class="pa-4">
+        <template v-if="!selection.length">
+            No file selected.
+        </template>
+        <template v-else>
+            <pre v-html="selection" />
+        </template>
     </v-main>
 </template>
 <script>
-import { mapStores, mapActions, mapState } from 'pinia';
-import { useRepoStore } from '@/src/stores/repoStore';
-// import FileListItem from './FileListItem.vue';
+    import {mapStores, mapActions, mapState} from 'pinia';
+    import {useRepoStore} from '@/src/stores/repoStore';
+    // import FileListItem from './FileListItem.vue';
 
-export default {
-    name: 'OverviewPage',
-    data() {
-        return {
-            repoLoading: false,
-            open: [],
-        }
-    },
-    computed: {
-        ...mapStores(useRepoStore),
-        ...mapState(useRepoStore, ['content', 'author', 'authorName', 'repoName'])
-    },
-    mounted() {
-        this.loadContent();
-    },
-    methods: {
-        ...mapActions(useRepoStore, ['setContent', 'updateContent', 'setAuthor']),
-        async loadContent() {
-            this.repoLoading = true;
-            
-            try {
-                const response = await fetch(`http://localhost:8000/getcontent/${this.authorName}/${this.repoName}`);
-                const data = await response.json();
-
-                this.setContent(data);
-            } catch (error) {
-                alert('Something wrong');
-                console.error(error);
+    export default {
+        name: 'OverviewPage',
+        data() {
+            return {
+                repoLoading: false,
+                open: [],
+                selection: '',
             }
-            this.repoLoading = false;
         },
-        // async.getFolder()
+        computed: {
+            ...mapStores(useRepoStore),
+            ...mapState(useRepoStore, ['content', 'author', 'authorName', 'repoName'])
+        },
+        mounted() {
+            this.loadContent();
+        },
+        methods: {
+            ...mapActions(useRepoStore, ['setContent', 'updateContent', 'setAuthor']),
+            async loadContent() {
+                this.repoLoading = true;
+
+                try {
+                    const response = await fetch(`http://localhost:8000/getcontent/${this.authorName}/${this.repoName}`);
+                    const data = await response.json();
+
+                    this.setContent(data);
+                } catch (error) {
+                    alert('Something wrong');
+                    console.error(error);
+                }
+                this.repoLoading = false;
+            },
+            async getFileContent(filename) {
+
+                try {
+                    const response = await fetch(`http://localhost:8000/getfile/${this.authorName}/${this.repoName}/${filename}`);
+                    const data = await response.json();
+
+                    console.log(data)
+                    this.selection = data;
+                } catch (error) {
+                    alert('Something wrong');
+                    console.error(error);
+                }
+            }
+        }
     }
-}
 </script>
